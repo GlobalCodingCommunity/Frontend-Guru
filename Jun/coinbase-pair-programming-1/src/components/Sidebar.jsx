51bd800a6f4051fcd721672f5990ae04655ddbe2
vancityjun <vancityjun@gmail.com>
2025-08-27 16:15:51 -0700

    solve coinbase part I

--- Jun/coinbase-pair-programming-1/src/components/Sidebar.jsx
+++ Jun/coinbase-pair-programming-1/src/components/Sidebar.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { fetchSidebarData } from '../utils/ApiClient';
 
 // TODO Q5
 const Sidebar = () => {
+  const [sidebarItems, setSidebarItems] = useState([]);
+
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        const results = await fetchSidebarData();
+        const list = [];
+
+        if (typeof results.count === 'number') {
+          for (let i = 1; i <= results.count; i++) {
+            const id = `line_${i}`;
+            const content = results[id];
+            if (content) {
+              list.push({ id, content });
+            }
+          }
+        }
+        setSidebarItems(list);
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+
+    loadData();
+  }, []);
+
   return (
     <div className="sidebar" data-testid="sidebar">
-      <div className="sidebar-item">test</div>
+      {sidebarItems.map(({ id, content }) => (
+        <div key={id} className="sidebar-item">
+          {content}
+        </div>
+      ))}
     </div>
   );
 };
