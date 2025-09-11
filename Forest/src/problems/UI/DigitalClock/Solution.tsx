f37b17b1768c2fcde772c06dccfffc02fd923272
Forest Park <foresthpark@gmail.com>
2025-09-10 22:27:17 -0700

    Update digital clock styling and time display

--- Forest/src/problems/UI/DigitalClock/Solution.tsx
+++ Forest/src/problems/UI/DigitalClock/Solution.tsx
@@ -11,16 +11,22 @@ export function Clock() {
       style={{
         display: "flex",
         flexDirection: "row",
-        border: "5px solid darkgray",
+        border: "1rem solid darkgray",
         width: "fit-content",
-        padding: "5px",
+        padding: "1rem",
         background: "black",
         borderRadius: "5px",
       }}
     >
-      <TimeComponent time={hours} semicolon />
-      <TimeComponent time={minutes} semicolon />
-      <TimeComponent time={seconds} />
+      {/** hour */}
+      <TimeComponent time={Math.floor(hours / 10)} />
+      <TimeComponent time={hours % 10} semicolon />
+      {/** minute */}
+      <TimeComponent time={Math.floor(minutes / 10)} />
+      <TimeComponent time={minutes % 10} semicolon />
+      {/** seconds */}
+      <TimeComponent time={Math.floor(seconds / 10)} />
+      <TimeComponent time={seconds % 10} />
     </div>
   );
 }
@@ -41,16 +47,22 @@ const useCurrentTime = () => {
   return date;
 };
 
-const TimeComponent = ({ time, semicolon = false }: { time: number; semicolon?: boolean }) => {
+const TimeComponent = ({
+  time,
+  semicolon = false,
+}: {
+  time: number;
+  semicolon?: boolean;
+}) => {
   return (
     <div
       style={{
         fontWeight: "bold",
-        fontSize: "3rem",
+        fontSize: "7rem",
         color: "white",
       }}
     >
-      {time.toString().padStart(2, "0")}
+      {time}
       {semicolon && `:`}
     </div>
   );
