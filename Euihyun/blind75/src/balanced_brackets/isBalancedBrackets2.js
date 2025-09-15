937b40cf4f629d3e7e049f3465810e2558399b36
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2025-09-15 12:18:46 -0600

    fix: refactor Blind75/Balanced Brackets

---
+++ Euihyun/blind75/src/balanced_brackets/isBalancedBrackets2.js
@@ -0,0 +1,40 @@
+// In order to submit to greatfrontend, change the type of str and all following variables to any
+function isBalancedBrackets(str) {
+  const bracketsStack = [];
+  const bracketMatch = {
+    "[": "]",
+    "{": "}",
+    "(": ")",
+  };
+
+  const isOpening = (char) => {
+    return bracketMatch[char];
+  };
+
+  for (const char of str) {
+    if (bracketsStack.length === 0) {
+      if (!isOpening(char)) return false;
+      else bracketsStack.push(char);
+    } else {
+      const topChar = bracketsStack[bracketsStack.length - 1];
+      if (topChar && !isOpening(topChar)) return false;
+      else {
+        if (isOpening(char)) {
+          bracketsStack.push(char);
+        } else if (isOpening(topChar) === char) {
+          bracketsStack.pop();
+        } else {
+          return false;
+        }
+      }
+    }
+  }
+
+  return bracketsStack.length === 0 ? true : false;
+}
+
+console.log(isBalancedBrackets("[]")); //true
+console.log(isBalancedBrackets("([)]")); //false
+console.log(isBalancedBrackets("([]){}")); //true
+console.log(isBalancedBrackets("](){}[")); //false
+console.log(isBalancedBrackets("(){}[]")); //true
