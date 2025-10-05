296352e771940b53590e2d988766eb06b8019a24
Forest Park <foresthpark@gmail.com>
2025-10-04 22:53:24 -0700

    Add ProgressBars2 Original and Solution components

---
+++ Forest/src/problems/UI/ProgressBars2/Solution.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles.css';
+
+export default function Solution() {
+  return (
+    <div className="container">
+      <h1>Progress Bars 2</h1>
+      {/* Your solution here */}
+    </div>
+  );
+}
